05c2ef8cba016add6742bdd734175ff28c41c926
migrenaa <mimonova13@gmail.com>
2021-04-19 16:11:54 +0300

    HSpace token is now native, added script for transfering the tokens to the storyInteraction contract

--- non-zone-contracts/migrations/3_NFTs.js
+++ non-zone-contracts/migrations/3_NFTs.js
@@ -1,22 +1,12 @@
 const StoryFactory = artifacts.require('StoryFactory');
 const StoryInteractionFactory = artifacts.require('StoryInteractionFactory');
-const HSpaceToken = artifacts.require('HSpaceToken');
 
-const DEPLOYER_ADDRESS = '0x2CEF62C91Dd92FC35f008D1d6Ed08EADF64306bc';
 const HOST = '0xEB796bdb90fFA0f28255275e16936D25d3418603';
 const CFA = '0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873';
-const SPACE_TOKEN_MUMBAI= '0xf91123D5e24913613206d4e2Facf2e024CC88bC0';
-// const STORY_FACTORY = '0x08D32D581847afD9F215CC212723cb477f8EF8d0';
-// unwrapped - 0x84742946013e04432615202d77b958A5F9538e72
- const STORY_FACTORY_MUMBAI = '0x3e19eE4B1EfF873c6132C7a5476f02F7E213eD4c';
-
+// TODO: change the decimals of the supertoken when deploying on matic => ideally 4
+const SPACE_TOKEN_MUMBAI= '0x5bc52B5c2d04ABf41c6AEfdb32862BEb50FABc68';
 
 module.exports = async (deployer, network, accounts) => {
-
-  // await deployer.deploy(
-  //   HSpaceToken,
-  //   500
-  // );
 
   await deployer.deploy(
     StoryFactory,
@@ -34,4 +24,7 @@ module.exports = async (deployer, network, accounts) => {
     SPACE_TOKEN_MUMBAI,
     StoryFactory.address
   );
+
+  
+
 };
---
+++ non-zone-contracts/transferTokens.js
@@ -0,0 +1,258 @@
+
+// Import the ethers library
+var ethers = require('ethers');
+require('dotenv').config()
+
+const STORY_INTERACTION_ADDRESS = '0xA72fD6f3753D5153023294224aaDc74e812be250';
+const abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+];
+const provider = new ethers.providers.JsonRpcProvider(
+    `https://rpc-mumbai.maticvigil.com/v1/${process.env.MATIC_KEY}`
+);
+
+// Wallet connected to a provider
+const senderWalletMnemonic = ethers.Wallet.fromMnemonic(
+    process.env.MNEMONIC,
+    "m/44'/60'/0'/0/0"
+);
+
+let signer = senderWalletMnemonic.connect(provider);
+
+let hSpaceTokenContract = new ethers.Contract(
+    process.env.HSPACE_TOKEN_ADDRESS,
+    abi,
+    signer,
+);
+
+hSpaceTokenContract.on(
+    'Transfer',
+    (from, to, value) => {
+        console.log('Trasnfer succeeded!');
+        console.log('from', from);
+        console.log('to', to);
+        console.log('value', value);
+    },
+);
+hSpaceTokenContract.transfer(STORY_INTERACTION_ADDRESS, ethers.utils.parseUnits('500', 18))
+.then(res => console.log(res))
+.catch(err => console.log(err));
